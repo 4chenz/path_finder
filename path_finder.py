593663fce1026cf302b63ffe84a7b65328a70868
4chenz <4chenz@users.noreply.github.com>
2017-05-15 04:05:29 -0600

    update

--- path_finder.py
+++ path_finder.py
@@ -1,5 +1,4 @@
 ## Me: nice.meme@getbackinthe.kitchen
-## Might kill it's self sometimes, not sure why. Probably because copy pasta.
 ## All filenames should be named the tracker and the same as in the lists.
 ## All tracker names should be the same across files.
 import os
